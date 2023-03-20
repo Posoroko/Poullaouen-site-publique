4927d2c6c9c6d9f85a1dfd94802fafeb09631af3
Posoroko <eric@posoroko.com>
2023-03-20 13:50:49 +0100

    console log clearance

--- components/Section/SectionShoeBox.vue
+++ components/Section/SectionShoeBox.vue
@@ -26,7 +26,6 @@ const appConfig = useAppConfig();
 const directusAssets = appConfig.directus.assets;
 
 const showInModal = (e) => {
-    console.log('eric')
 
     const modal = document.getElementById('masterModal')
     let children = modal.children
