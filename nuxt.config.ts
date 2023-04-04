cf96d9b0f620a49890077562a0c4ddaaac24a4eb
Posoroko <eric@posoroko.com>
2023-04-04 10:50:24 +0200

    sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,48 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-configconsole
 export default defineNuxtConfig({
+    runtimeConfig: {
+        public: {
+            titleSeparator: '|',
+            siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://mairie-poullaouen.fr',
+            siteName: 'Mairie de Poullaouen',
+            siteDescription: 'Site officiel de la mairie de Pouallaouen',
+            language: 'fr-FR',
+        }
+    },
     css: [
         '@/assets/css/colors.css',
         '@/assets/css/reset.css',
         '@/assets/css/fonts.css',
         '@/assets/css/main.css',
-        '@/assets/css/utility.css',
-        // '@/assets/css/userActions.css',
+        '@/assets/css/utility.css'
     ],
     ssr: true,
 
     modules: [
         "nuxt-directus"
     ],
+    extends: [
+        'nuxt-seo-kit'
+    ],
     directus: {
         url: "https://admin.mairie-poullaouen.fr"
     },
     app: {
         head: {
-            htmlAttrs: {
-                lang: 'fr'
-            },
-            link: [
-                {
-                    rel: 'icon',
-                    type: 'image/x-icon',
-                    href: '/images/logo-square.jpg',
-                },
-            ],
-            charset: 'utf-8',
-            viewport: 'width=500, initial-scale=1',
-            title: 'Poullaouen',
-            meta: [
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { name: 'description', content: 'Site de la Mairie de Poullaouen.' },
-                { name: "og:description", content: 'Site de la Mairie de Poullaouen.' },
-                { name: "og:site_name", content: "Mairie de Poullaouen" },
-                { name: "og:type", content: "website" },
-                { name: "og:image", content: "Poullaouen" },
-                { name: "og:url", content: "https://mairie-poullaouen.fr" },
-                { name: "google-site-verification", content: "sMTScukwNBaazph3D2m7z4y0_-M1EH8L1C1n2dYdxeQ" }
-            ],
-        },
-        // baseURL: '/accueil/'
+            titleTemplate: '%pageTitle %titleSeparator %siteName'
+        }
     }
-    
 })
