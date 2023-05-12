726b9dea0c0fed984319d7afd98092d6d211d738
Posoroko <eric@posoroko.com>
2023-05-12 10:04:46 +0200

    photo sorting

--- pages/galerie-photo/index.vue
+++ pages/galerie-photo/index.vue
@@ -137,7 +137,7 @@ const fetchOptions = {
 const { data: galeryData } = await useAsyncData(
     "galery",
     async () => {
-        const items = await $fetch(`${directusItems}Albums_photo?[filter][gallery][_eq]=true`, fetchOptions)
+        const items = await $fetch(`${directusItems}Albums_photo?[filter][gallery][_eq]=true&sort[]=-date_created`, fetchOptions)
         const albums = items.data
 
         const temp = {
